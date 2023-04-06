2bc9cf96abbc16a5032bfd0f38f8586dd3d67714
UnerDarks <gsoaresribeiro40@gmail.com>
2023-04-06 16:40:14 -0300

    horarios

--- scripts/cidade.js
+++ scripts/cidade.js
@@ -96,28 +96,36 @@ function Horarios() {
   var data = new Date();
   var diaDaSemana = data.getDay();
 
+  // Objeto que contém as informações dos horários para cada dia da semana e para cada div
+  var horariosObj = {
+    "horariosDiv1": {
+      //DOMINGO
+      "0": "<li>5:50</li><li>08:10</li><li>10:30</li><li>12:20</li><li>14:30</li><li>16:30</li><li>18:30</li>",
+      //SABADO
+      "6": "<li>10:30</li><li>13:00</li><li>15:00</li><li>17:00</li>",
+      //DIAS ULTEIS
+      "default": "<li>05:30</li><li>07:10</li><li>07:50</li><li>09:50</li><li>11:50</li><li>13:50</li><li>15:50</li><li>17:50</li><li>18:50</li><li>21:30</li>"
+    },
+    "horariosDiv2": {
+      "0": "<li>6:00</li><li>09:00</li><li>12:00</li>",
+      "6": "<li>11:00</li><li>14:00</li><li>17:00</li>",
+      "default": "<li>06:00</li><li>07:30</li><li>09:00</li><li>10:30</li><li>12:00</li><li>13:30</li><li>15:00</li><li>16:30</li><li>18:00</li><li>19:30</li><li>21:00</li>"
+    }
+  };
+
   function imprimirHorario(id) {
     var horarios = document.getElementById(id);
-    switch (diaDaSemana) {
-      case 0: // Domingo
-        horarios.innerHTML = "<li>5:50</li><li>08:10</li><li>10:30</li><li>12:20</li><li>14:30</li><li>16:30</li><li>18:30</li>";
-        break;
-      case 6: // Sábado
-        horarios.innerHTML = "<li>10:30</li><li>13:00</li><li>15:00</li><li>17:00</li>";
-        break;
-      default: // Demais dias da semana
-        horarios.innerHTML = "<li>05:30</li><li>07:10</li><li>07:50</li><li>09:50</li><li>11:50</li><li>13:50</li><li>15:50</li><li>17:50</li><li>18:50</li><li>21:30</li>";
-        break;
-    }
+    var horariosParaDia = horariosObj[id][diaDaSemana] || horariosObj[id]["default"];
+    horarios.innerHTML = horariosParaDia;
   }
-  
+
   document.getElementById("botao1").addEventListener("click", function() {
     imprimirHorario("horariosDiv1");
   });
-  
+
   document.getElementById("botao2").addEventListener("click", function() {
     imprimirHorario("horariosDiv2");
   });
 }
 
-Horarios();
+Horarios(); 
